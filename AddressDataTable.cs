72a9cd8ef8eceae8f755979f16e3fe0aaffe647c
kandula-prakash <abhinayprakash143@gmail.com>
2021-09-12 23:14:56 +0530

    feat:AdderssBook

---
+++ AddressDataTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace AddressBookDemo
+{
+    class AddressDataTable
+    {
+        public DataTable AddressBookDataTable()
+        {
+            DataTable table = new DataTable();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+
+namespace AddressBookDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book Program\n");
+
+            // uc-1
+            AddressDataTable addressBook = new AddressDataTable();
+            DataTable dataTable = addressBook.AddressBookDataTable();
+
+            // uc-2
+            //addressBook.EditContact(dataTable);
+
+            // uc-3
+            //addressBook.DeleteContact(dataTable);
+
+            // uc-4
+            //addressBook.RetrieveByCityOrState(dataTable);
+
+            // uc-7
+           // addressBook.GetSizeOfCityOrState(dataTable);
+
+            // uc-6
+          //  addressBook.SortContacts(dataTable);
+
+            // uc-10
+          //  addressBook.GetCountByType(dataTable);
+        }
+    }
+}
+        
+    
+
