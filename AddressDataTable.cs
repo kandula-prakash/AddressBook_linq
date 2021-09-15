b515996015da9118d7d90ebc6b75e7853b5fd57d
kandula-prakash <abhinayprakash143@gmail.com>
2021-09-15 13:29:49 +0530

    feat:addressBook

--- AddressDataTable.cs
+++ AddressDataTable.cs
@@ -8,9 +8,24 @@ namespace AddressBookDemo
 {
     class AddressDataTable
     {
+        //uc1
         public DataTable AddressBookDataTable()
         {
             DataTable table = new DataTable();
+
+
+            //uc2
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+
+            return table;
         }
     }
-}
+ }
+
